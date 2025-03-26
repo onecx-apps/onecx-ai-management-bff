4a6c3dbcf132269f0e3cb3f981c2b8d172f0ef56
Tchana D. <ruphus.tchana-djeuji@capgemini.com>
2025-03-26 12:55:41 +0100

    feat: create feature implemented in bff

--- src/main/java/org/tkit/onecx/ai/bff/rs/mappers/AIKnowledgeDocumentMapper.java
+++ src/main/java/org/tkit/onecx/ai/bff/rs/mappers/AIKnowledgeDocumentMapper.java
@@ -5,10 +5,11 @@ import org.mapstruct.Mapping;
 import org.tkit.quarkus.rs.mappers.OffsetDateTimeMapper;
 
 import gen.org.tkit.onecx.ai.bff.rs.internal.model.AIKnowledgeDocumentDTO;
+import gen.org.tkit.onecx.ai.bff.rs.internal.model.CreateAIKnowledgeDocumentDTO;
 import gen.org.tkit.onecx.ai.bff.rs.internal.model.ProblemDetailResponseDTO;
 import gen.org.tkit.onecx.ai.bff.rs.internal.model.UpdateAIKnowledgeDocumentDTO;
 import gen.org.tkit.onecx.ai.mgmt.client.model.AIKnowledgeDocument;
-//import gen.org.tkit.onecx.ai.mgmt.client.model.CreateAIKnowledgeDocumentRequest;
+import gen.org.tkit.onecx.ai.mgmt.client.model.CreateAIKnowledgeDocumentRequest;
 import gen.org.tkit.onecx.ai.mgmt.client.model.UpdateAIKnowledgeDocumentRequest;
 import gen.org.tkit.onecx.permission.model.ProblemDetailResponse;
 
@@ -20,14 +21,15 @@ import gen.org.tkit.onecx.permission.model.ProblemDetailResponse;
 @Mapper(uses = { OffsetDateTimeMapper.class })
 public interface AIKnowledgeDocumentMapper {
 
-    //        @Mapping(target = "modificationCount", ignore = true)
     AIKnowledgeDocumentDTO mapById(AIKnowledgeDocument aiKnowledgeDocument);
 
     UpdateAIKnowledgeDocumentRequest mapUpdate(UpdateAIKnowledgeDocumentDTO updateAIKnowledgeDocumentDTO);
 
-    // CreateAIKnowledgeDocumentRequest mapCreate(CreateAIKnowledgeDocumentDTO createAIKnowledgeDocumentDTO);
+    @Mapping(target = "name", source = "aIKnowledgeDocumentData.name")
+    @Mapping(target = "status", source = "aIKnowledgeDocumentData.status")
+    @Mapping(target = "documentRefId", source = "aIKnowledgeDocumentData.documentRefId")
+    CreateAIKnowledgeDocumentRequest mapCreate(CreateAIKnowledgeDocumentDTO createAIKnowledgeDocumentDTO);
 
-    //
     @Mapping(target = "removeParamsItem", ignore = true)
     ProblemDetailResponseDTO mapErrorDetails(ProblemDetailResponse problemDetailResponse);
 }
